719b479839c32d4d1a3a80fd61ab03c6e1d8db53
sonyamonyaa <74297783+sonyamonyaa@users.noreply.github.com>
2021-12-06 16:21:48 +0200

    Test

--- Tests/NodeTest.java
+++ Tests/NodeTest.java
@@ -1,44 +1,55 @@
+import Imp.Node;
+import Imp.geoLocation;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
+import static org.junit.jupiter.api.Assertions.*;
 class NodeTest {
 
+    private Node n;
+    geoLocation g;
+    @BeforeEach
+    void setUp(){
+        g = new geoLocation(1,1,0);
+        n = new Node(1,g,0,"og node");
+    }
     @Test
-    void setKey() {
+    void Key() {
+        assertEquals(1,n.getKey());
+        n.setKey(2);
+        assertEquals(2,n.getKey());
     }
 
     @Test
-    void getKey() {
+    void Location() {
+        geoLocation p1 = (geoLocation) n.getLocation();
+        assertTrue(p1.x() == g.x() && p1.y() == g.y() && p1.z() == g.z());
+        geoLocation p2 = new geoLocation(2,2,2);
+        n.setLocation(p2);
+        p1 = (geoLocation) n.getLocation();
+        assertTrue(p1.x() == p2.x() && p1.y() == p2.y() && p1.z() == p2.z());
     }
 
     @Test
-    void getLocation() {
+    void Weight() {
+        assertEquals(0.0,n.getWeight());//default
+        n.setWeight(2.1);
+        assertEquals(2.1,n.getWeight());
     }
 
     @Test
-    void setLocation() {
+    void Info() {
+        String str = "og node";
+        assertEquals(str,n.getInfo());
+        str = "new info";
+        n.setInfo(str);
+        assertEquals(str,n.getInfo());
     }
 
     @Test
-    void getWeight() {
+    void Tag() {
+        assertEquals(0,n.getTag());
+        n.setTag(1);
+        assertEquals(1,n.getTag());
     }
 
-    @Test
-    void setWeight() {
-    }
-
-    @Test
-    void getInfo() {
-    }
-
-    @Test
-    void setInfo() {
-    }
-
-    @Test
-    void getTag() {
-    }
-
-    @Test
-    void setTag() {
-    }
 }
