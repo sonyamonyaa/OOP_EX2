9b23a63ffb458a51518bd94d171725fe4a4aae5f
Israel Jacobovitz <learsi0545488592@gmail.com>
2021-12-07 22:32:36 +0200

    load function

---
+++ Tests/JasonConversionTest.java
@@ -0,0 +1,13 @@
+import Imp.*;
+import org.junit.jupiter.api.Test;
+
+
+class GraphTest {
+    static String Path = "C:/Users/ישראל/IdeaProjects/Ex2/data/G1.json";
+
+    public static void main(String[] args) {
+
+
+
+    }
+}
