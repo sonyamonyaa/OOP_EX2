89f69a5da9802f72063207500ea5a8c2e836c5a8
sonyamonyaa <74297783+sonyamonyaa@users.noreply.github.com>
2021-12-13 14:41:18 +0200

    added remove node test for edges

--- Tests/EdgesTest.java
+++ Tests/EdgesTest.java
@@ -9,13 +9,13 @@ import static org.junit.jupiter.api.Assertions.*;
  * This test is specifically for the Edges class, as it is in a protected package we are using Graph
  * as its edge methods are directly tied to the Edges class
  */
-class EdgesTest {private Edge e1 = new Edge(0,1,1.232037506070033);
-    private Edge e2 = new Edge(1,2,1.8015954015822042);
-    private Edge e3 = new Edge(1,0,1.8635670623870366);
-    private Node n1 = new Node(0,new geoLocation(35.19589389346247,32.10152879327731,0.0));
-    private Node n2 = new Node(1,new geoLocation(35.20319591121872,32.10318254621849,0.0));
-    private Node n3 = new Node(2,new geoLocation(35.20752617756255,32.1025646605042,0.0));
-    private Graph g = new Graph(3);
+class EdgesTest {private final Edge e1 = new Edge(0,1,1.232037506070033);
+    private final Edge e2 = new Edge(1,2,1.8015954015822042);
+    private final Edge e3 = new Edge(1,0,1.8635670623870366);
+    private final Node n1 = new Node(0,new geoLocation(35.19589389346247,32.10152879327731,0.0));
+    private final Node n2 = new Node(1,new geoLocation(35.20319591121872,32.10318254621849,0.0));
+    private final Node n3 = new Node(2,new geoLocation(35.20752617756255,32.1025646605042,0.0));
+    private final Graph g = new Graph(3);
 
     @BeforeEach
     void setUp(){
@@ -40,15 +40,17 @@ class EdgesTest {private Edge e1 = new Edge(0,1,1.232037506070033);
         g.connect(e1.getSrc(),e1.getDest(),3);
         assertEquals(1,g.edgeSize()); //should stay the same even tho the weight changed
     }
-
     @Test
-    void edgeIter() {
-    }
+    void removeNode(){
+        g.connect(e1.getSrc(),e1.getDest(),e1.getWeight());
+        g.connect(e2.getSrc(),e2.getDest(),e2.getWeight());
+        g.connect(e3.getSrc(),e3.getDest(),e3.getWeight());
+        assertEquals(3,g.edgeSize());
+        g.removeNode(1);
+        assertEquals(2, g.nodeSize());
+        assertEquals(0,g.edgeSize());// :(
 
-    @Test
-    void testEdgeIter() {
     }
-
 
     @Test
     void removeEdge() {
